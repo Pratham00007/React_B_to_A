9fc3feaee5a94e69d5da558dd21ac1ec285a29bd
Pratham <pratham10600@gmail.com>
2025-10-28 08:18:58 +0530

    formated code

--- fifth/src/UserGreeting.jsx
+++ fifth/src/UserGreeting.jsx
@@ -1,6 +1,16 @@
 function UserGreeting(props){
-    return(props.isLoggedIn ?   <h2 className="welcome-message">Welcome {props.username}</h2>
-                            :   <h2 className="login-prompt">Please login to continue</h2>
+
+    const welcomeWessage = <h2 className="welcome-message">
+                            Welcome {props.username}
+                            </h2>
+    
+    const loginPrompt = <h2 className="login-prompt">
+                        Please login to continue
+                        </h2>
+
+
+    return(props.isLoggedIn ?  welcomeWessage 
+                            :  loginPrompt 
     )
 }
 
